a6f51ce803f23803881be46517012eb101ee9316
Iramal <9588504@gmail.com>
2020-08-06 00:32:00 +0500

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-
 user = User.create(login: 'Jhon')
 article = Article.create(title: 'В Калуге обнаружены Бобры', body: 'Бобры добры')
 video = Video.create(title: 'Видео про бобров', video_url: 'https://www.youtube.com/watch?v=uq1vIwUjowc')
 
-continent  = Location.create(name: 'Евразия')
+continent = Location.create(name: 'Евразия')
 country = Location.create(name: 'Россия', attachment: continent)
 region = Location.create(name: 'ЦФО', attachment: country)
 district = Location.create(name: 'Калужская область', attachment: region)
 city = Location.create(name: 'Калуга', article: article, attachment: district)
 
-
-comment = Comment.create(user: user, commentable: article, body: 'Бобры комментируют')
-comment2 = Comment.create(user: user, commentable: video, body: 'Коммент под бобровский видос')
+Comment.create(user: user, commentable: article, body: 'Бобры комментируют')
+Comment.create(user: user, commentable: video, body: 'Коммент под бобровский видос')
