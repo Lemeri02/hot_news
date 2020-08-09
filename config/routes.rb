611b57b6cf29a51718ad8b51d3bf976eab640222
Iramal <9588504@gmail.com>
2020-08-09 21:46:48 +0500

    add gem & set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    api_actions = [:index, :show, :create, :update, :destroy]
+
+    concern :commentable do
+      resources :comments, only: [:index, :create, :update, :destroy]
+    end
+
+    resources :articles, only: api_actions, concerns: [:commentable]
+    resources :locations, only: api_actions do
+      resources :articles, only: [:index]
+    end
+    resources :users, only: api_actions
+    resources :videos, only: api_actions, concerns: [:commentable]
+  end
+
+  resource :session, only: [:show, :create, :destroy]
 end
